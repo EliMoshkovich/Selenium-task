dfc181d89157a5e12be120ab84887fc74bb49827
EliMoshkovich <tbhjfo14@gmail.com>
2019-03-23 18:20:49 +0200

    Add files via upload

--- SeleniumTask.py
+++ SeleniumTask.py
@@ -5,26 +5,20 @@ from selenium.webdriver.common.keys import Keys
 
 
 
-def driverInitialize():
+def driverInitialize(driver_path):
 
     "Please replace the driver_path below according to your own driver location"
-
-    # driver path
-    driver_path = "chromedriver/chromedriver.exe"
 
     # driver can by changed according to browser driver
     driver = webdriver.Chrome(driver_path)
     return driver
 
-def resultsNumber(driver):
+def resultsNumber(driver,search_string,search_engine):
 
     "Function will search claroty at google and print to console results number"
 
-    # Can be changed if need
-    search_string = "Claroty"
-
     # open google
-    driver.get("https://www.google.com/")
+    driver.get(search_engine)
     # locate the search field
     search = driver.find_element_by_name('q')
     # type "Claroty" into search field
@@ -38,29 +32,28 @@ def resultsNumber(driver):
     # print the text of resultStats
     print("Results number by google search for Claroty is about: " , res_dig)
 
-def firstLink(driver):
+def firstLink(driver , url_first_link):
 
     "Function will check if claroty.com is the first results link"
-
-    # URL can be changed if need
-    url = "https://www.claroty.com/"
 
     # finds webresults
     links_list = driver.find_elements_by_xpath('//div[@class="r"]/a/h3')
     # clicks the first link
     links_list[0].click()
     # check if link is claroty.com
-    if driver.current_url == url:
+    if driver.current_url == url_first_link:
         print(driver.current_url , 'Is the first link of google search results')
     else:
         print("https://www.claroty.com/  Is not the first link of google search results")
 
-def clarotyJobs(driver):
+def clarotyJobs(driver,  careers_url):
 
     "Function will check how many visible jobs exist at careers page"
 
-    driver.get("https://www.claroty.com/careers")
+    driver.get(careers_url)
+    # find class name of jobs in our web
     elements = driver.find_elements_by_class_name('w-dyn-item')
+    # sub function that calculating the number of the visible jobs only
     numberOfVisibleItems = 0
     for item in elements:
         children = item.find_elements_by_xpath('div')
--- main.py
+++ main.py
@@ -3,12 +3,27 @@ from SeleniumTask import *
 
 def main():
 
-    " Starting the task  "
+    " For Generic program, the values are here and can be changed as needed "
 
-    browser_driver = driverInitialize()
-    resultsNumber(browser_driver)
-    firstLink(browser_driver)
-    clarotyJobs(browser_driver)
+    # browser driver path
+    driver_path = "chromedriver/chromedriver.exe"
+
+    # what to search
+    search_string = "Claroty"
+    # search engine website
+    search_engine = "https://www.google.com/"
+
+    # URL of our website
+    url_first_link = "https://www.claroty.com/"
+
+    # URL for claroty's careers
+    careers_url = "https://www.claroty.com/careers"
+
+    # All the program's functions
+    browser_driver = driverInitialize(driver_path)
+    resultsNumber(browser_driver, search_string, search_engine)
+    firstLink(browser_driver, url_first_link)
+    clarotyJobs(browser_driver,  careers_url)
     exit(browser_driver)
 
 
